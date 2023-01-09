014411ec72b6765274123b8277f252f2dacdebab
Thanachok Rattanamora <schweppeweb@gmail.com>
2023-01-09 15:06:09 +0700

    Initial Commit

---
+++ iamrich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by Schweppe on 9/1/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
